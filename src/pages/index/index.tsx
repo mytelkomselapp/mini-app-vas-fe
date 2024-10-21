919103e6541eb7c41fd1a6540b9de3b500783836
RamdlanFaqih <ramfaqih@gmail.com>
2024-10-21 20:26:22 +0700

    revert navigation test

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,15 +2,9 @@ import { Button, Text, View } from "@tarojs/components";
 import Taro from "@tarojs/taro";
 
 const Index = () => {
-  const navigation = () => {
-    Taro.navigateTo({
-      url: '/pages/ListPenerbangan/index'
-    })
-  }
   return (
     <View>
       <Text>Hello world!</Text>
-      <Button onClick={navigation}>Navigate</Button>
     </View>
   );
 };
