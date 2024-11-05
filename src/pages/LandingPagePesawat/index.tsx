871f87d130ca071710c4a7c8c5312192f256fb4b
imkamalh <imkamalh@gmail.com>
2024-11-05 16:54:33 +0700

    init: LandingPage

--- src/pages/LandingPagePesawat/index.tsx
+++ src/pages/LandingPagePesawat/index.tsx
@@ -1,33 +1,7 @@
-import { View, Text, Button } from "@tarojs/components";
-import Taro from "@tarojs/taro";
-import { useLoad } from "@tarojs/taro";
-
-import { useFetchCMSLandingPage } from "../../network";
+import LandingPagePesawat from "./LandingPagePesawat";
 
 const Index = () => {
-  useLoad(() => {
-    console.log("Page loaded.");
-  });
-
-  const navigateToLandingPagePesawat = () => {
-    Taro.navigateTo({ url: "/pages/landingPagePesawat/index" });
-  };
-
-  const { data } = useFetchCMSLandingPage();
-
-  console.log(data);
-
-  return (
-    <View className="index">
-      <div>
-        <h2>span</h2>
-      </div>
-      <Text>Hello world!</Text>
-      <Button onClick={navigateToLandingPagePesawat}>
-        Go to Landing Page Pesawat
-      </Button>
-    </View>
-  );
+  return <LandingPagePesawat />;
 };
 
 export default Index;
