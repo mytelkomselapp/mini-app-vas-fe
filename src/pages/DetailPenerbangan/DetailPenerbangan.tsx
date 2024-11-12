9001684093aff994c137b49b0c2cb35ac90c79dc
imkamalh <imkamalh@gmail.com>
2024-11-12 15:56:26 +0700

    feat: navigation popup

--- src/pages/DetailPenerbangan/DetailPenerbangan.tsx
+++ src/pages/DetailPenerbangan/DetailPenerbangan.tsx
@@ -2,8 +2,8 @@
 import { Button, Image, Switch } from "@tarojs/components";
 import { useCallback, useEffect, useMemo, useState } from "react";
 // import Navbar, { NavColor } from "../..//../components/Navbar";
-import ChevronRight from "../../assets/chevron-right.svg"
-import TicketUpload from "../../assets/icon-ticket-upload.svg"
+import ChevronRight from "../../assets/chevron-right.svg";
+import TicketUpload from "../../assets/icon-ticket-upload.svg";
 import IconDelete from "../../assets/ico-delete.svg";
 import IconSuccess from "../../assets/ico_success_filled.svg";
 // import { ReactComponent as ChevronRight } from "../..//../assets/chevron-right.svg";
@@ -13,7 +13,7 @@ import FlightBoardInfo from "../../modules/FlightBoardInfo";
 // import { FreemiumLimitModal } from "../../modules/FlightForm";
 import FlightDetailsCard from "../../modules/FlightDetailsCard";
 import Show from "../../components/Show";
-import { getNavigateState } from "../../lib/utils";
+import { getNavigateState, handleNavigate } from "../../lib/utils";
 import Taro from "@tarojs/taro";
 
 // import {
@@ -30,11 +30,23 @@ import { buttonClick, screenView } from "../../network/analytics/tracker";
 // import { FlightDetailRawData } from "@/network/types/response-props";
 // import { useCallback, useEffect, useMemo } from "react";
 // import { useSearchParams, useLocation, useNavigate } from "react-router-dom";
-import { useFlightTicketForm, useSaveTrackingPayload } from "../../store/flight";
+import {
+  useFlightTicketForm,
+  useSaveTrackingPayload,
+} from "../../store/flight";
 // import Show from "../..//../components/Show";
 import moment from "moment";
 import useToggle from "../../hooks/useToggle";
-import { useDeleteETicket, useFetchETicketByFlightId, useFetchFlightDetail, useFetchFlightTrack, useFetchFreemiumPackage, useFollowFlight, usePostClaimFreeTicket, useUnfollowFlight } from "../../network";
+import {
+  useDeleteETicket,
+  useFetchETicketByFlightId,
+  useFetchFlightDetail,
+  useFetchFlightTrack,
+  useFetchFreemiumPackage,
+  useFollowFlight,
+  usePostClaimFreeTicket,
+  useUnfollowFlight,
+} from "../../network";
 import { FlightDetailRawData } from "../../network/types/response-props";
 import { FreemiumLimitModal } from "../../modules/FlightForm";
 // import useToggle from "@/hooks/useToggle";
@@ -50,7 +62,7 @@ const USER_FREEMIUM_QUOTA = 5;
 const DetailPesawat = () => {
   const searchParams = Taro.getCurrentInstance().router?.params;
 
-  const currentPath = Taro.getCurrentInstance().router?.path || '';
+  const currentPath = Taro.getCurrentInstance().router?.path || "";
   const state = useMemo(() => getNavigateState(currentPath), [currentPath]);
 
   const { active: visibleOffer, toggleActive: toggleOffer } = useToggle();
@@ -85,7 +97,7 @@ const DetailPesawat = () => {
     setPlaneNo,
     resetFlightTicketState,
   } = useFlightTicketForm();
-  
+
   const { mutateAsync: postFollowFlight, isLoading: loadingFollowFlight } =
     useFollowFlight();
 
@@ -98,7 +110,7 @@ const DetailPesawat = () => {
     refetch: refetchTrackedFlight,
     isFetching: loadingFetchTrackedFlight,
   } = useFetchFlightTrack();
-  
+
   const { data: dataUserPackage } = useUserPackageStatus(!loadingFollowFlight);
   const dataTrackedFlight =
     rawDataTrackedFlight?.data?.data?.tracked_flights ?? [];
@@ -111,7 +123,7 @@ const DetailPesawat = () => {
     const followedFlight = [...dataTrackedFlight]?.find(
       (data) =>
         String(data?.flight?.flight_no)?.toUpperCase() ===
-        String(flightNumber)?.toUpperCase() &&
+          String(flightNumber)?.toUpperCase() &&
         flightDate === moment(data?.flight?.departure_time).format("YYYY-MM-DD")
     );
 
@@ -157,8 +169,8 @@ const DetailPesawat = () => {
     const flightId = dataFollowFlight?.data?.flight_id;
 
     Taro.navigateTo({
-      url: "/pages/CreateDetailTicket/index/"+ flightId,
-    }); 
+      url: "/pages/CreateDetailTicket/index/" + flightId,
+    });
   };
 
   useEffect(() => {
@@ -184,10 +196,8 @@ const DetailPesawat = () => {
     fetchingFlightDetail;
 
   const onProceedPackage = () => {
-    // navigate({
-    //   pathname: "/flight/package",
-    // });
-  }
+    handleNavigate("/pages/SubscriptionPackage/index");
+  };
 
   const getLimitType = () => {
     const packageNameUser = dataUserPackage?.package_name;
@@ -219,7 +229,12 @@ const DetailPesawat = () => {
         buttonClick("Follow Flight", "Follow Flight", "Detail", "/detail");
         toast({
           title: "Notifikasi penerbangan ini telah diaktifkan",
-          prefixComponent: <Image src={IconSuccess} style={{ width: '24px', height: '24px' }} />,
+          prefixComponent: (
+            <Image
+              src={IconSuccess}
+              style={{ width: "24px", height: "24px" }}
+            />
+          ),
           className:
             "bg-white rounded-full [&_div]:font-normal [&>button]:hidden fixed !top-8 z-[100] flex max-h-screen flex-col-reverse sm:top-auto sm:flex-col max-w-[90vw] !left-0 !right-0 px-4 py-3 m-auto",
           duration: 3000,
@@ -280,49 +295,49 @@ const DetailPesawat = () => {
   const { mutateAsync: deleteETicket, isLoading: loadingDeleteETicket } =
     useDeleteETicket();
 
-    const handleDelete = async () => {
-      const eTicketId = eTicketData?.data?.data?.id;
-      if (eTicketId) {
-        const deleteETicketData = await deleteETicket({ id: eTicketId });
-        if (deleteETicketData?.data?.meta?.status === "success") {
-          removeETicketQuery();
-        } else {
-          toast({
-            title: "Uh oh! Something went wrong.",
-            description: "There was a problem with your request.",
-            className: "bg-[#fef2f4] text-solidRed",
-            duration: 3000,
-          });
-        }
-      }
-    };
-  
-    const handleOpenTicket = () => {
-      const eTicket = eTicketData?.data?.data;
-      if (!eTicket?.file_url) return;
-  
-      const snackTicketName = eTicket?.ticket_name
-        ?.split(" ")
-        ?.join("_")
-        ?.toLowerCase();
-  
-      // TODO: check this
-      // return navigate("/flight/file-preview", {
-      //   state: {
-      //     fileUrl: eTicket?.file_url,
-      //     fileExt: eTicket?.file_ext,
-      //     fileName: snackTicketName,
-      //   },
-      // });
-    };
-  
-    const handleFollowSwitch = () => {
-      if (dataFollowFlight?.isFollowing) {
-        handleUnfollowFlight();
+  const handleDelete = async () => {
+    const eTicketId = eTicketData?.data?.data?.id;
+    if (eTicketId) {
+      const deleteETicketData = await deleteETicket({ id: eTicketId });
+      if (deleteETicketData?.data?.meta?.status === "success") {
+        removeETicketQuery();
       } else {
-        handleFollowFlight();
+        toast({
+          title: "Uh oh! Something went wrong.",
+          description: "There was a problem with your request.",
+          className: "bg-[#fef2f4] text-solidRed",
+          duration: 3000,
+        });
       }
-    };
+    }
+  };
+
+  const handleOpenTicket = () => {
+    const eTicket = eTicketData?.data?.data;
+    if (!eTicket?.file_url) return;
+
+    const snackTicketName = eTicket?.ticket_name
+      ?.split(" ")
+      ?.join("_")
+      ?.toLowerCase();
+
+    // TODO: check this
+    // return navigate("/flight/file-preview", {
+    //   state: {
+    //     fileUrl: eTicket?.file_url,
+    //     fileExt: eTicket?.file_ext,
+    //     fileName: snackTicketName,
+    //   },
+    // });
+  };
+
+  const handleFollowSwitch = () => {
+    if (dataFollowFlight?.isFollowing) {
+      handleUnfollowFlight();
+    } else {
+      handleFollowFlight();
+    }
+  };
 
   return (
     <>
@@ -337,11 +352,11 @@ const DetailPesawat = () => {
           <div className="flex flex-col gap-y-2 rounded-[16px] bg-white min-h-[50px] p-3 text-left shadow-[0_10px_34px_rgba(0,0,0,0.1)]">
             <div className="flex justify-between items-center">
               <span className="text-sm font-semibold">Detail E-Ticket</span>
-              <Image 
-                src={ChevronRight} 
+              <Image
+                src={ChevronRight}
                 style={{
-                  width: '24px',
-                  height: '24px'
+                  width: "24px",
+                  height: "24px",
                 }}
                 onClick={handleOpenTicket}
               />
@@ -349,11 +364,11 @@ const DetailPesawat = () => {
 
             <div className="flex items-center justify-between">
               <div className="flex gap-x-4 items-center">
-                <Image 
-                  src={TicketUpload} 
+                <Image
+                  src={TicketUpload}
                   style={{
-                    width: '48px',
-                    height: '49px'
+                    width: "48px",
+                    height: "49px",
                   }}
                 />
                 <div className="flex flex-col justify-between gap-y-1">
@@ -374,15 +389,15 @@ const DetailPesawat = () => {
               </div>
 
               <Show when={!loadingDeleteETicket}>
-                  <Image 
-                    src={IconDelete} 
-                    style={{
-                      width: '24px',
-                      height: '24px'
-                    }}
-                    onClick={handleDelete}
-                  />
-                </Show>
+                <Image
+                  src={IconDelete}
+                  style={{
+                    width: "24px",
+                    height: "24px",
+                  }}
+                  onClick={handleDelete}
+                />
+              </Show>
             </div>
           </div>
         </Show>
@@ -391,12 +406,20 @@ const DetailPesawat = () => {
           <div className="flex flex-col gap-y-2 rounded-[16px] bg-white min-h-[50px] mt-4 p-3 text-left shadow-[0_10px_34px_rgba(0,0,0,0.1)]">
             <div className="flex justify-between items-center">
               <span className="text-sm">Dapatkan Notifikasi Penerbangan</span>
-              <Switch checked={dataFollowFlight?.isFollowing} onChange={handleFollowSwitch} color="#001A41"/>
+              <Switch
+                checked={dataFollowFlight?.isFollowing}
+                onChange={handleFollowSwitch}
+                color="#001A41"
+              />
             </div>
 
             <div className="flex items-center justify-between">
               <div className="flex gap-x-4 items-center">
-                <span className="text-xs">Terima notifikasi real-time mulai dari check-in hingga pengambilan bagasi, agar perjalanan Anda lebih lancar dan teratur.</span>
+                <span className="text-xs">
+                  Terima notifikasi real-time mulai dari check-in hingga
+                  pengambilan bagasi, agar perjalanan Anda lebih lancar dan
+                  teratur.
+                </span>
               </div>
             </div>
           </div>
@@ -417,11 +440,11 @@ const DetailPesawat = () => {
                 }}
               >
                 <span className="text-sm">Tambah Tiket&nbsp;</span>
-                <Image 
-                  src={IconPlus} 
+                <Image
+                  src={IconPlus}
                   style={{
-                    width: '24px',
-                    height: '24px'
+                    width: "24px",
+                    height: "24px",
                   }}
                 />
               </button>
