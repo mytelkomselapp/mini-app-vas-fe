f4d46c3166e8bb9eff0a0a697cdf81c9cbd99d0b
imkamalh <imkamalh@gmail.com>
2024-11-25 14:55:16 +0700

    feat: extendData

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Taro from "@tarojs/taro";
 import { withProvider } from "./hoc";
 import "./index.css";
 import "./app.scss";
@@ -8,6 +9,21 @@ interface AppProps {
 }
 
 class App extends React.Component<AppProps> {
+  componentDidMount() {
+    this.onLaunch();
+  }
+
+  onLaunch() {
+    // Retrieve extendData from the launch options
+    const launchOptions = Taro.getLaunchOptionsSync();
+    const extendData = launchOptions?.query?.extendData;
+
+    console.log("App launched with extendData:", extendData);
+    // if (extendData) {
+    //   Taro.setStorageSync("customParams", extendData);
+    // }
+  }
+
   render() {
     return this.props.children;
   }
