70bfc8fea187246a3e8f698a3a92dd09f17a205c
RamdlanFaqih <ramfaqih@gmail.com>
2025-01-30 00:18:35 +0700

    fix: custom radio button ui

--- src/pages/PrayerSchedule/components/RamadhanReminderSetting/ReminderSetting.tsx
+++ src/pages/PrayerSchedule/components/RamadhanReminderSetting/ReminderSetting.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
+import { RadioGroup } from "@tarojs/components";
 import Button from "../../../../components/Button";
 import Unmute from "../../../../assets/ico-unmute.svg";
 import Stop from "../../../../assets/ico-stop.svg";
 import Notification from "../../../../assets/ico-notification-black.svg";
 import { PrayerStatus, usePrayerNotification } from "../../../../store/ramadhan";
 import Chips from "../../../../components/Chips";
+import './ReminderSetting.scss'
 
 interface ReminderSettingProps {
     id: number | undefined;
@@ -16,18 +18,17 @@ const ReminderSetting = ({ id, currentStatus, toggleModal }: ReminderSettingProp
     const [selectedOption, setSelectedOption] = useState<PrayerStatus>(currentStatus);
     const { updatePrayerStatus } = usePrayerNotification();
 
-    
     const handleOptionChange = (value: PrayerStatus) => {
         setSelectedOption(value);
     };
-    
+
     const handleSubmit = () => {
         if (id !== undefined) {
             updatePrayerStatus(id, selectedOption);
             toggleModal();
-        } 
+        }
     };
-    
+
     useEffect(() => {
         setSelectedOption(currentStatus);
     }, [currentStatus]);
@@ -35,7 +36,7 @@ const ReminderSetting = ({ id, currentStatus, toggleModal }: ReminderSettingProp
     return (
         <>
             <div className='text-center text-base font-semibold'>Pengingat Waktu Subuh</div>
-            <div className='flex flex-col'>
+            <RadioGroup className='flex flex-col'>
                 {[
                     { label: "Suara Adzan", value: "adzan", icon: Unmute },
                     { label: "Suara Bedug", value: "bedug", icon: Unmute },
@@ -50,26 +51,21 @@ const ReminderSetting = ({ id, currentStatus, toggleModal }: ReminderSettingProp
                             <img src={option.icon} alt={option.label} style={{ width: '24px', height: '24px' }} />
                             <span className='text-base py-4'>{option.label}</span>
                         </div>
-                        <input
-                          type='radio'
-                          color='black'
-                          name='reminder'
-                          value={option.value}
-                          checked={selectedOption === option.value}
-                          onChange={() => handleOptionChange(option.value as PrayerStatus)}
-                          className=' accent-black w-5 h-5 appearance-none'
-                        />
+                        <div
+                          className={`custom-radio-button ${selectedOption === option.value ? 'selected' : ''}`}
+                          onClick={() => handleOptionChange(option.value as PrayerStatus)}
+                        >
+                            <div className={`checked-dot ${selectedOption === option.value ? 'checked' : ''}`}></div>
+                        </div>
                     </label>
                 ))}
-            </div>
+            </RadioGroup>
             <div className='mt-4'>
                 <div className='flex justify-between'>
                     <span className='text-sm font-semibold text-primaryBlack'>Pengingat Sebelum Subuh</span>
                     <label className='switch'>
                         <input
                           type='checkbox'
-                        //   checked={!!isActive}
-                        //   onChange={toggleNotification}
                         />
                         <span className='slider'></span>
                     </label>
