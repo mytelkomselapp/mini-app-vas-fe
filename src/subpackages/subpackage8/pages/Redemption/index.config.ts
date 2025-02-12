92f3269784b105ba724b1f2c82943fd3b98e0f87
imkamalh <imkamalh@gmail.com>
2025-02-12 12:06:43 +0700

    style: status penukaran

---
+++ src/subpackages/subpackage8/pages/Redemption/index.config.ts
@@ -0,0 +1,6 @@
+export default definePageConfig({
+  navigationBarTitleText: "Status Penukaran",
+  backgroundTextStyle: "black",
+  navigationBarBackgroundColor: "white",
+  navigationBarTextStyle: "black",
+});
---
+++ src/subpackages/subpackage8/pages/Redemption/index.tsx
@@ -0,0 +1,7 @@
+import Redemption from "./Redemption";
+
+const Index = () => {
+  return <Redemption />;
+};
+
+export default Index;
